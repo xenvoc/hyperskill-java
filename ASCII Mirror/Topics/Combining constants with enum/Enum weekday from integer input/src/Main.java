cd4fbcad7a7c3dea1a311e61de798ea4ddff8e37
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-09-15 16:24:25 +0300

    Do Combining constants with enum

---
+++ ASCII Mirror/Topics/Combining constants with enum/Enum weekday from integer input/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int dayNumber = scanner.nextInt();
+
+        Weekday day = Weekday.values()[dayNumber - 1];
+        System.out.println(day);
+    }
+}
+
+enum Weekday {
+    // Define the enum constants here
+    MONDAY, TUESDAY, WEDNESDAY, THURDSAY, FRIDAY, SATURDAY, SUNDAY
+}

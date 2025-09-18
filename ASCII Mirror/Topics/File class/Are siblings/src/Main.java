9eaa31068a6dfc72d83b475e85cc41d8be816bc7
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-09-18 18:01:52 +0300

    Do Are siblings?

---
+++ ASCII Mirror/Topics/File class/Are siblings/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+class Siblings {
+
+    public static boolean areSiblings(File f1, File f2) {
+        // implement me
+        if (f1.getParent().equals(f2.getParent())) return true;
+        else return false;
+    }
+}

0304f074aedfe82c69cc860982a07af1773281ab
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-10-28 18:28:17 +0200

    Do Simple Maven Project

---
+++ Assigned but no Solve in IDE button/Simple Maven Project/first-maven-app/src/main/java/com/hyperskill/App.java
@@ -0,0 +1,13 @@
+package com.hyperskill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

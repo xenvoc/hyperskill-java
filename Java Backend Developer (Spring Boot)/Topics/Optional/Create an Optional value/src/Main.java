5e352d0dbe685f9483e46172420517c039147193
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-11-02 18:06:48 +0200

    Do Create an Optional value

---
+++ Java Backend Developer (Spring Boot)/Topics/Optional/Create an Optional value/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Optional;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        InputStringReader reader = new InputStringReader();        
+        Optional<String> value = reader.getValue();
+        if (value.isPresent()) {
+            System.out.println("Value is present: " + value.get());
+        } else {
+            System.out.println("Value is empty");
+        }
+    }
+}
+
+class InputStringReader {
+    public Optional<String> getValue() {
+        // implement
+        Scanner reader = new Scanner(System.in);
+        String input = reader.next();
+        if (input.equals("empty")) return Optional.empty();
+        else return Optional.of(input);
+    }
+}

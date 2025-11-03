56257cfdf37932c421b36acfb17afffc4f743963
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-11-03 20:57:10 +0200

    Do Which Class?

---
+++ Assigned but no Solve in IDE button/Which class/src/WhichClass.java
@@ -0,0 +1,8 @@
+import java.util.Set;
+
+public class WhichClass {
+    public static void main(String[] args) {
+        Set experiment = Set.of(5, 3, 2);
+    }
+}
+// ImmutableCollections$SetN@830, so SetN

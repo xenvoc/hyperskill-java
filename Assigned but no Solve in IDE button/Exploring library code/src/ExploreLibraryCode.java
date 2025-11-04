6cd21aa8570f90d912ee0525af5764e4c179d236
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-11-04 03:26:36 +0200

    Do Exploring library code

---
+++ Assigned but no Solve in IDE button/Exploring library code/src/ExploreLibraryCode.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExploreLibraryCode {
+    public static void main(String[] args) {
+        ArrayList<Integer> dummy = new ArrayList<Integer> ();
+        dummy.removeAll(List.of(5,3,2));
+    }
+}
+// it's batchRemove, found with step into

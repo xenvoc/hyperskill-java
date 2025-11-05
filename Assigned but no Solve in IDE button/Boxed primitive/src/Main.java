bd57f973310bec970a11e9a579fa9de01669a89f
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-11-05 17:01:22 +0200

    Do Boxed primitive

---
+++ Assigned but no Solve in IDE button/Boxed primitive/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+      List<Integer> dummy = new ArrayList<Integer>();
+      dummy.add(1);
+    }
+}
+
+// Integer.valueOf

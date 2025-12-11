5c2506988e33b27be76b371e215fe00667cda39f
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-11 19:11:23 +0200

    Type dependencyless Spring Hello World

---
+++ Assigned but no Solve in IDE button/Spring Hello World/src/DemoApplication.java
@@ -0,0 +1,14 @@
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}

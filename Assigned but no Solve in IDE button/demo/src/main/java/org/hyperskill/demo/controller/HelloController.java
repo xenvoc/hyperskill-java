0289bf74db8ecdc3cb64f06c10c3b4b7822e924b
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-17 19:49:10 +0200

    Produce Hello World on localhost on demo2

---
+++ Assigned but no Solve in IDE button/demo/src/main/java/org/hyperskill/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.hyperskill.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}

738273f7e916bbfeb40b9e674d60293a22dedd6d
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-21 18:28:04 +0200

    Add query parameter in demo2

--- Assigned but no Solve in IDE button/demo2/src/main/java/org/hyperskill/demo/controller/HelloController.java
+++ Assigned but no Solve in IDE button/demo2/src/main/java/org/hyperskill/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package org.hyperskill.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -17,5 +18,16 @@ public class HelloController {
     @GetMapping("/about")
     public String about() {
         return "Website for Spring Boot testing";
+    }
+
+    @GetMapping("cooking")
+    public String cooking(@RequestParam String item) {
+        char fLetter = item.charAt(0);
+        // minutes to cook item:
+        int minutes = -1;
+        if (fLetter < 'o')
+            minutes = 15;
+        else minutes = 35;
+        return "Cook " + item + " for " + minutes + " minutes.";
     }
 }

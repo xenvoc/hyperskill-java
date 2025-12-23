48959328b1c693cbc8cee0c88eb0ef2c288f0cf2
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-23 12:40:37 +0200

    Make a Maven Spring boot CLI app

---
+++ Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CliExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hane.cli_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliExampleApplication.class, args);
+	}
+
+}
---
+++ Assigned but no Solve in IDE button/cli_example/src/test/java/com/hane/cli_example/CliExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hane.cli_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

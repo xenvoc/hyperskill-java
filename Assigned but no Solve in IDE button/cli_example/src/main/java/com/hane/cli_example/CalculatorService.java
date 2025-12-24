d6cb3ce2eec6ae7a41a38fe52aa928f39e7c2f85
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-24 16:39:19 +0200

    Add Calculator Service to Spring example app

---
+++ Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CalculatorService.java
@@ -0,0 +1,13 @@
+package com.hane.cli_example;
+
+import org.springframework.stereotype.Service;
+
+class CalculatorService {
+    public int sum(int... numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
+}

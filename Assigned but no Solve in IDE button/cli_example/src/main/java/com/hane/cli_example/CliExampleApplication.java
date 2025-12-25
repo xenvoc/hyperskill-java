0eb5247f3b45bfc98c317ca8a4d48e8e842e229a
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-25 14:00:48 +0200

    Add CommandLineRunner

--- Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CliExampleApplication.java
+++ Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CliExampleApplication.java
@@ -2,12 +2,40 @@ package com.hane.cli_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.bean.factory.annotation.Autowired;
+
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class CliExampleApplication {
+    @AutoWired
+    private CalculatorService calculatorService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CliExampleApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter numbers separated by spaces:");
+            String input = scanner.nextLine();
+            String[] numbersStr = input.split(" ");
+            int[] numbers = new int[numbersStr.length];
+            for (int i = 0; i < numbersStr.length; i++) {
+                try {
+                    numbers[i] = Integer.parseInt(numbersStr[i]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid number: " + numbersStr[i]);
+                }
+            }
+            int sum = calculatorService.sum(numbers);
+            System.out.println("The sum of the numbers is: " + sum);
+        };
+    }
 
 }

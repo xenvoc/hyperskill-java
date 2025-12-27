9aabfe14afdfb7955f5be22332bf2c2fa9e43b6d
xenvoc <131164834+xenvoc@users.noreply.github.com>
2025-12-27 16:53:23 +0200

    Fix bean import and more

--- Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CliExampleApplication.java
+++ Assigned but no Solve in IDE button/cli_example/src/main/java/com/hane/cli_example/CliExampleApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.bean.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 
 import java.util.Scanner;

b0dab111bd157bcb39e646d92d0735f3a5344492
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-01-13 14:44:57 +0200

    Do Lists - Select correct assignments

---
+++ Assigned but no Solve in IDE button/Lists - Select correct assignments/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Collections<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new ArrayList<>();
+        LinkedList<Integer> list4 = new LinkedList<>(new ArrayList<>());
+        //LinkedList<Integer> list5 = new ArrayList<>();
+    }
+}

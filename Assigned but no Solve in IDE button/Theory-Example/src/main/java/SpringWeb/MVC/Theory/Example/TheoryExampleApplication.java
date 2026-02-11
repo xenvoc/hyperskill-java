e009b0e141afa632278f218de8c808870dd67efa
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-02-11 18:17:50 +0200

    Initializr Spring Web MVC Theory Example

---
+++ Assigned but no Solve in IDE button/Theory-Example/src/main/java/SpringWeb/MVC/Theory/Example/TheoryExampleApplication.java
@@ -0,0 +1,13 @@
+package SpringWeb.MVC.Theory.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheoryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheoryExampleApplication.class, args);
+	}
+
+}
---
+++ Assigned but no Solve in IDE button/Theory-Example/src/test/java/SpringWeb/MVC/Theory/Example/TheoryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringWeb.MVC.Theory.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheoryExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

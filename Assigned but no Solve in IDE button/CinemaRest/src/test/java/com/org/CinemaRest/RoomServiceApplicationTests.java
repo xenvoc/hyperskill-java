9bfdef42cedc987eb407ec23b7d7678e1c33583e
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-11 21:01:26 +0200

    Initializr Cinema Rest Room Service base

---
+++ Assigned but no Solve in IDE button/CinemaRest/src/test/java/com/org/CinemaRest/RoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.CinemaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

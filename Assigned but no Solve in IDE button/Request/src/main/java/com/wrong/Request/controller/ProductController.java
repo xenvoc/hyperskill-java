15790ea6336d67887644d3036379fa4416d744cf
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-19 10:45:05 +0200

    Fill up ProductController

--- Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/controller/ProductController.java
+++ Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/controller/ProductController.java
@@ -1,4 +1,14 @@
 package com.wrong.Request.controller;
 
 public class ProductController {
+    private final List<Product> productList = List.of(
+            new Product(1, "Milk"),
+            new Product(2, "Sausages"),
+            new Product(3, "Butter")
+    );
+
+    @GetMapping("/products/{id}")
+    public Product getProduct(@PathVariable int id) {
+        return productList.get(id - 1);
+    }
 }

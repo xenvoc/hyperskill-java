947c94e6a332f0cdb5702acfeefe08ae5af626cd
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-21 10:30:21 +0200

    Add product getters, setters, constructor

--- Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
+++ Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
@@ -1,4 +1,24 @@
 package com.wrong.Request.model;
 
 public class Product {
+
+    private int id;
+    private String name;
+
+    //constructors, getters, and setters
+    Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id;}
+    public String getName() { return name; }
+
+    public void setId(int id) { this.id = id;}
+    public String setName(String name) { this.name = name;}
+
+
+
+
+
 }

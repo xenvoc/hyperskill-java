d0104b77457d3be91227b666661aec265bbcca13
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-23 10:50:14 +0200

    Make model public

--- Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
+++ Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
@@ -6,7 +6,7 @@ public class Product {
     private String name;
 
     //constructors, getters, and setters
-    Product(int id, String name) {
+    public Product(int id, String name) {
         this.id = id;
         this.name = name;
     }
@@ -14,9 +14,8 @@ public class Product {
     public int getId() { return id;}
     public String getName() { return name; }
 
-    public void setId(int id) { this.id = id;}
-    public String setName(String name) { this.name = name;}
-
+    public void setId(int id) { this.id = id; }
+    public String setName(String name) { this.name = name; }
 
 
 

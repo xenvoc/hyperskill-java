ebdb7d50c620331fa23d2562fb3ce0b65ea20e0c
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-26 09:53:23 +0200

    Fix wrong return on method

--- Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
+++ Assigned but no Solve in IDE button/Request/src/main/java/com/wrong/Request/model/Product.java
@@ -15,7 +15,7 @@ public class Product {
     public String getName() { return name; }
 
     public void setId(int id) { this.id = id; }
-    public String setName(String name) { this.name = name; }
+    public void setName(String name) { this.name = name; }
 
 
 

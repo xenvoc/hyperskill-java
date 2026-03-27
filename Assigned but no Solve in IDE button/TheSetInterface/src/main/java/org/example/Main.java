93b792fb63374035525d3cef49da801d39abad87
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-27 15:51:52 +0200

    Do The Set Interface exercise

---
+++ Assigned but no Solve in IDE button/TheSetInterface/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String>  emptySet = Set.of();
+        Set<String> people = Set.of("Larry", "Kenny", "Sabrina");
+        Set<Integer> numbers = Set.of(100, 200, 300, 400);
+
+    }
+}

f35dcbd569d142a1f9cd6d42493b47f976c2905e
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-28 17:03:59 +0200

    Add prints in TheSetInterface

--- Assigned but no Solve in IDE button/TheSetInterface/src/main/java/org/example/Main.java
+++ Assigned but no Solve in IDE button/TheSetInterface/src/main/java/org/example/Main.java
@@ -8,5 +8,9 @@ public class Main {
         Set<String> people = Set.of("Larry", "Kenny", "Sabrina");
         Set<Integer> numbers = Set.of(100, 200, 300, 400);
 
+        System.out.println(emptySet);
+        System.out.println(people);
+        System.out.println(numbers);
+
     }
 }

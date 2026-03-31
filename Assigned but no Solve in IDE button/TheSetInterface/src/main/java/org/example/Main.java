623b5cda65186e17ef52ed7fffbb842d46b9bf06
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-03-31 17:44:55 +0300

    Test TreeSet functions in TheSetInterface

--- Assigned but no Solve in IDE button/TheSetInterface/src/main/java/org/example/Main.java
+++ Assigned but no Solve in IDE button/TheSetInterface/src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,6 +30,26 @@ public class Main {
 
         System.out.println(countries);
         System.out.println(countries.contains("Switzerland"));
+
+        SortedSet<Integer> sortedSet = new TreeSet<>();
+
+        sortedSet.add(10);
+        sortedSet.add(15);
+        sortedSet.add(13);
+        sortedSet.add(21);
+        sortedSet.add(17);
+
+        System.out.println(sortedSet);
+
+        System.out.println(sortedSet.headSet(15));
+        System.out.println(sortedSet.tailSet(15));
+
+        System.out.println(sortedSet.subSet(13, 17));
+
+        System.out.println(sortedSet.first());
+        System.out.println(sortedSet.last());
+
+
 
     }
 }

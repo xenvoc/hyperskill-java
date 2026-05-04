773cf521e18780f1ca60b0b1f4bdb03c1891b563
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-05-04 15:31:59 +0300

    Make tesla bean primary

--- Assigned but no Solve in IDE button/DependencyInjectionSpring/src/main/java/org/spring/dependencyinjectionspring/model/Config.java
+++ Assigned but no Solve in IDE button/DependencyInjectionSpring/src/main/java/org/spring/dependencyinjectionspring/model/Config.java
@@ -2,11 +2,13 @@ package org.spring.dependencyinjectionspring.model;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class Config {
 
     @Bean
+    @Primary
     public Car teslaCar() {
         return new Car("Tesla", "2023");
     }

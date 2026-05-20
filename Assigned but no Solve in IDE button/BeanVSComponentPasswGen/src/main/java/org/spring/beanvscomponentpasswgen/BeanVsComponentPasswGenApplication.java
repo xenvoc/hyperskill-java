645f01929bc3c772e2317a492a35ae5b67fb6517
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-05-20 15:28:54 +0300

    Generate Maven Spring project for BeanVSComponent

---
+++ Assigned but no Solve in IDE button/BeanVSComponentPasswGen/src/main/java/org/spring/beanvscomponentpasswgen/BeanVsComponentPasswGenApplication.java
@@ -0,0 +1,13 @@
+package org.spring.beanvscomponentpasswgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanVsComponentPasswGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanVsComponentPasswGenApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/BeanVSComponentPasswGen/src/test/java/org/spring/beanvscomponentpasswgen/BeanVsComponentPasswGenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.beanvscomponentpasswgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanVsComponentPasswGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3e80be4de1a7cf43e1e760bff7c824668363da1c
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-05-24 16:35:49 +0300

    Generate Spring app for Spring Stereotypes

---
+++ Assigned but no Solve in IDE button/StereotypesSpring/src/test/java/org/spring/stereotypesspring/StereotypesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.stereotypesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StereotypesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

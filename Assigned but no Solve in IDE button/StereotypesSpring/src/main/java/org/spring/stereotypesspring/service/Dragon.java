196fc1f90ca96b98221936b17d0733a73ffc7708
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-05-26 14:35:41 +0300

    Add Dragon service @Component

---
+++ Assigned but no Solve in IDE button/StereotypesSpring/src/main/java/org/spring/stereotypesspring/service/Dragon.java
@@ -0,0 +1,11 @@
+package org.spring.stereotypesspring.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dragon {
+
+    public String slayDragon() {
+        return "Rawr or smth";
+    }
+}

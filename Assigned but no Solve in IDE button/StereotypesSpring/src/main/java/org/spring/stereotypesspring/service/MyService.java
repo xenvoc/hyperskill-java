6b4a0cb836804d9be2c3131132df521fff0d7597
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-04 15:47:35 +0300

    Add MyService

---
+++ Assigned but no Solve in IDE button/StereotypesSpring/src/main/java/org/spring/stereotypesspring/service/MyService.java
@@ -0,0 +1,14 @@
+package org.spring.stereotypesspring.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    private String generatePassowordHash(String userPassword) {
+        // dummy process
+        String hashedPassword = "youWontGuessIt";
+        return hashedPassword;
+    }
+
+}

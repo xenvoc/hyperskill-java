e40d3753ac2799d3febb40358e46256cf643040d
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-06 15:07:40 +0300

    Generate Spring for Springs of Beans

---
+++ Assigned but no Solve in IDE button/Spring/ScopesOfBeans/src/main/java/org/spring/scopesofbeans/ScopesOfBeansApplication.java
@@ -0,0 +1,13 @@
+package org.spring.scopesofbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopesOfBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScopesOfBeansApplication.class, args);
+    }
+
+}

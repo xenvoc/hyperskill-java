5228d58df248b18cd61e7e701b10655b6690c343
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-11 14:51:03 +0300

    Generate Spring for Bean Lifecycle

---
+++ Assigned but no Solve in IDE button/Spring/BeanLifecycle/src/test/java/org/spring/beanlifecycle/BeanLifecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.beanlifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanLifecycleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

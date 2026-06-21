7a8c04778caeb2fb289b82c6b6c012c30f65a87b
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-21 15:25:03 +0300

    Get Person object

--- Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/ApplicationContextApplication.java
+++ Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/ApplicationContextApplication.java
@@ -1,6 +1,7 @@
 package org.spring.applicationcontext;
 
 import org.spring.applicationcontext.config.Config;
+import org.spring.applicationcontext.model.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,6 +14,7 @@ public class ApplicationContextApplication {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(Config.class);
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        System.out.println(context.getBean(Person.class));
         //SpringApplication.run(ApplicationContextApplication.class, args);
     }
 

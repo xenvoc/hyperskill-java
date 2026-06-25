ccc194ec6febc7e852d87f7bac8c691c7f4fc754
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-25 11:47:25 +0300

    Add ComponentScan to Config

--- Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/config/Config.java
+++ Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/config/Config.java
@@ -2,8 +2,10 @@ package org.spring.applicationcontext.config;
 
 import org.spring.applicationcontext.model.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@ComponentScan
 @Configuration
 public class Config {
 

faa1145bbb34efe3da8abf2767fb365821603293
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-29 10:21:34 +0300

    Move BeanDefinitionName fetch to after run

--- Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/ApplicationContextApplication.java
+++ Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/ApplicationContextApplication.java
@@ -13,9 +13,9 @@ public class ApplicationContextApplication {
 
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(Config.class);
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
         System.out.println(context.getBean(Person.class));
         SpringApplication.run(ApplicationContextApplication.class, args);
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
         System.out.println(context.getBean("personMay"));
         System.out.println(context.getBean("personMay", Person.class));
     }

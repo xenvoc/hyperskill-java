23e11f6f5b52e831e304cd9819bf0364397e4b78
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-06-30 11:10:31 +0300

    Add Runner

---
+++ Assigned but no Solve in IDE button/Spring/ApplicationContext/src/main/java/org/spring/applicationcontext/Runner.java
@@ -0,0 +1,19 @@
+package org.spring.applicationcontext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}

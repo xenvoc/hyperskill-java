8aede6bc00efbac6c17f1e30b35017a2539e8eed
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-02 13:40:36 +0300

    Generate Spring Boot for ReturnTheBean

---
+++ Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
@@ -0,0 +1,13 @@
+package org.spring.returnthebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnTheBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnTheBeanApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/test/java/org/spring/returnthebean/ReturnTheBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.returnthebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnTheBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6f6139412e0049acde58c813438d88ce64743e5f
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-04 15:03:49 +0300

    Comment out the Run

--- Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
+++ Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReturnTheBeanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReturnTheBeanApplication.class, args);
+        //SpringApplication.run(ReturnTheBeanApplication.class, args);
     }
 
 }

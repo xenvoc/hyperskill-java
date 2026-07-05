64cb12701b9e77027e55aa1e0aad2983bec879f9
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-05 14:04:38 +0300

    Add context to main application

--- Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
+++ Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
@@ -1,13 +1,16 @@
 package org.spring.returnthebean;
 
+import org.spring.returnthebean.Config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ReturnTheBeanApplication {
 
     public static void main(String[] args) {
         //SpringApplication.run(ReturnTheBeanApplication.class, args);
+        var context = new AnnotationConfigApplicationContext(Config.class);
     }
 
 }

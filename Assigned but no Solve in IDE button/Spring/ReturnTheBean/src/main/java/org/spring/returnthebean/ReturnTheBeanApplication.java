c7b64475dc66c7bfae1f5cc8479b995dbc6fd4d7
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-06 14:52:24 +0300

    Get bean1

--- Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
+++ Assigned but no Solve in IDE button/Spring/ReturnTheBean/src/main/java/org/spring/returnthebean/ReturnTheBeanApplication.java
@@ -11,6 +11,7 @@ public class ReturnTheBeanApplication {
     public static void main(String[] args) {
         //SpringApplication.run(ReturnTheBeanApplication.class, args);
         var context = new AnnotationConfigApplicationContext(Config.class);
+        System.out.println(context.getBean("bean1"));
     }
 
 }

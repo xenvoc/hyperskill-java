0e637a8151c77afe948ebd2b4068a02811b1df34
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-11 16:53:53 +0300

    Generate Spring Boot for External Resources

---
+++ Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
@@ -0,0 +1,13 @@
+package org.spring.externalresourcesdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalResourcesDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalResourcesDemoAppApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/test/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.externalresourcesdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalResourcesDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

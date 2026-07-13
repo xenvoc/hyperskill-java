abae88010438244d08479ec8ffadaa412559712f
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-13 16:38:01 +0300

    Set up main to use application.properties

--- Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
+++ Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
@@ -1,13 +1,27 @@
 package org.spring.externalresourcesdemoapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
-public class ExternalResourcesDemoAppApplication {
+public class ExternalResourcesDemoAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(ExternalResourcesDemoAppApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("One dollar = " + environment.getProperty("yen") + " Yen");
+        System.out.println("One dollar = " + environment.getProperty("EGP") + " EGP");
     }
 
 }

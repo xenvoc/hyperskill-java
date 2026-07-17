c6c7740d8631fef3ef23de577bfc3fce6e3a3d65
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-17 15:03:19 +0300

    Provide PropertySource

--- Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
+++ Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
+@PropertySource("classpath:user.properties")
 public class ExternalResourcesDemoAppApplication implements CommandLineRunner {
 
     @Autowired

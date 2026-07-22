5a15370a1b3e849cb49430a0d0f41c737b0f190e
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-22 14:32:01 +0300

    Add PropertySources together

--- Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
+++ Assigned but no Solve in IDE button/Spring/ExternalResourcesDemoApp/src/main/java/org/spring/externalresourcesdemoapp/ExternalResourcesDemoAppApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
-@PropertySource("classpath:user.properties")
-@PropertySource("classpath:landmark.properties")
+@PropertySources({
+        @PropertySource("classpath:user.properties"),
+        @PropertySource("classpath:landmark.properties")
+
+})
 public class ExternalResourcesDemoAppApplication implements CommandLineRunner {
 
     @Autowired

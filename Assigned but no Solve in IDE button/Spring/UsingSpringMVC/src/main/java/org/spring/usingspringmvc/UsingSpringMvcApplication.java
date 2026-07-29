f8a09669beaadcb383ba346092cb6ea3f3922ae8
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-29 17:14:13 +0300

    Generate Spring Web for UsingSpringMVC

---
+++ Assigned but no Solve in IDE button/Spring/UsingSpringMVC/src/main/java/org/spring/usingspringmvc/UsingSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.spring.usingspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingSpringMvcApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/UsingSpringMVC/src/test/java/org/spring/usingspringmvc/UsingSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.usingspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

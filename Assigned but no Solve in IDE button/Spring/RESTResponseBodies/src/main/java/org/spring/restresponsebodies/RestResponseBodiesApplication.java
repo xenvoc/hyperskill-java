7c22dfbeb0b68650f11404e320ed918a2f04d47d
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-07-31 14:23:33 +0300

    Generate Spring for REST Reponse Bodies

---
+++ Assigned but no Solve in IDE button/Spring/RESTResponseBodies/src/main/java/org/spring/restresponsebodies/RestResponseBodiesApplication.java
@@ -0,0 +1,13 @@
+package org.spring.restresponsebodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestResponseBodiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestResponseBodiesApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/RESTResponseBodies/src/test/java/org/spring/restresponsebodies/RestResponseBodiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.restresponsebodies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestResponseBodiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6b3186b7fd2ecfd0d41c1624fed66f2fa5902392
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-04 13:36:21 +0300

    Add imports for Mapping and Controller

--- Assigned but no Solve in IDE button/Spring/RESTResponseBodies/src/main/java/org/spring/restresponsebodies/controller/DemoController.java
+++ Assigned but no Solve in IDE button/Spring/RESTResponseBodies/src/main/java/org/spring/restresponsebodies/controller/DemoController.java
@@ -1,5 +1,8 @@
 package org.spring.restresponsebodies.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class DemoController {
 

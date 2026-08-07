c35639a4cddf12fbddd183bff0b21e582203f4b9
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-07 17:06:33 +0300

    Gen Spring for Posting and deleting data via REST

---
+++ Assigned but no Solve in IDE button/Spring/PostnDelREST/src/main/java/org/spring/postndelrest/PostnDelRestApplication.java
@@ -0,0 +1,13 @@
+package org.spring.postndelrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostnDelRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostnDelRestApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/PostnDelREST/src/test/java/org/spring/postndelrest/PostnDelRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.postndelrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostnDelRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

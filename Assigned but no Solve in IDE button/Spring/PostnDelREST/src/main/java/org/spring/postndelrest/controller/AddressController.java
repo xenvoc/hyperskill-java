ed9108172426d1bcf3283bc90d9bf7a6d17e8793
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-09 16:31:40 +0300

    Add @RequestParam in /addr

--- Assigned but no Solve in IDE button/Spring/PostnDelREST/src/main/java/org/spring/postndelrest/controller/AddressController.java
+++ Assigned but no Solve in IDE button/Spring/PostnDelREST/src/main/java/org/spring/postndelrest/controller/AddressController.java
@@ -1,5 +1,7 @@
 package org.spring.postndelrest.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,5 +10,10 @@ import java.util.concurrent.ConcurrentMap;
 @RestController
 public class AddressController {
     private ConcurrentMap<String, String> addrBook = new ConcurrentHashMap<>();
+
+    @PostMapping("/addr")
+    public void postAddress(@RequestParam String name, @RequestParam String addr) {
+        addrBook.put(name, addr);
+    }
 
 }

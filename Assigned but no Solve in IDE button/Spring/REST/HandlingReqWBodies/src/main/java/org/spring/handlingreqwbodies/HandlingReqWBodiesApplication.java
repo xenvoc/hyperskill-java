06925f255685e332b021e8f997c74506d6c1a4cb
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-13 15:22:36 +0300

    Generate Spring for Handling Requests With Bodies

---
+++ Assigned but no Solve in IDE button/Spring/REST/HandlingReqWBodies/src/main/java/org/spring/handlingreqwbodies/HandlingReqWBodiesApplication.java
@@ -0,0 +1,13 @@
+package org.spring.handlingreqwbodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingReqWBodiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlingReqWBodiesApplication.class, args);
+    }
+
+}

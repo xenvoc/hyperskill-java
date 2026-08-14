5d729b00e91d37d4260148a068da33fcf37baf88
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-14 15:42:50 +0300

    Add UserInfo model class

---
+++ Assigned but no Solve in IDE button/Spring/REST/HandlingReqWBodies/src/main/java/org/spring/handlingreqwbodies/model/UserInfo.java
@@ -0,0 +1,10 @@
+package org.spring.handlingreqwbodies.model;
+
+public class UserInfo {
+    private int id;
+    private String name;
+    private String phone;
+    private boolean enable;
+    
+    UserInfo() {}
+}

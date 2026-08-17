7d87bbe5778dad7177ddba354eef3f0e53379515
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-17 17:39:38 +0300

    Make /user return a list of users

--- Assigned but no Solve in IDE button/Spring/REST/HandlingReqWBodies/src/main/java/org/spring/handlingreqwbodies/controller/UserInfoController.java
+++ Assigned but no Solve in IDE button/Spring/REST/HandlingReqWBodies/src/main/java/org/spring/handlingreqwbodies/controller/UserInfoController.java
@@ -5,15 +5,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UserInfoController {
 
     @PostMapping("/user")
-    public String userStatus(@RequestBody UserInfo user) {
-        if (user.isEnabled()) {
-            return String.format("Hello! %s. Your account is enable nao.", user.getName());
-        } else {
-            return String.format("Hello! Nice to see you, %s! Your acc has been disabled", user.getName());
-        }
+    public String userStatus(@RequestBody List<UserInfo> userList) {
+        return String.format("Added %d users", userList.size());
     }
 }

778e0a244367f869fbe3585526df627557a3292b
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-19 14:41:03 +0300

    Generate Spring Boot Web for Sending Files

---
+++ Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/main/java/org/spring/servingfiles/ServingFilesApplication.java
@@ -0,0 +1,13 @@
+package org.spring.servingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingFilesApplication.class, args);
+    }
+
+}
---
+++ Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/test/java/org/spring/servingfiles/ServingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.servingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

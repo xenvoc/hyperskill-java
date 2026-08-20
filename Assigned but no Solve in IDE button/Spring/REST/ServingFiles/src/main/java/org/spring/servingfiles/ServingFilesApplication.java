2bc9aa8bd5d43a87027200aaeb0eef5074bf3a55
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-20 16:32:44 +0300

    Add file path

--- Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/main/java/org/spring/servingfiles/ServingFilesApplication.java
+++ Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/main/java/org/spring/servingfiles/ServingFilesApplication.java
@@ -3,11 +3,14 @@ package org.spring.servingfiles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
 public class ServingFilesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServingFilesApplication.class, args);
+        Path path = Path.of("files", "example.txt");
     }
 
 }

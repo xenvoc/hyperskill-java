5c71f40576d70ccdef51e9af299eeb1230f605df
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-08-23 16:59:14 +0300

    Complete FileController with PathResource

--- Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/main/java/org/spring/servingfiles/controller/FileController.java
+++ Assigned but no Solve in IDE button/Spring/REST/ServingFiles/src/main/java/org/spring/servingfiles/controller/FileController.java
@@ -1,4 +1,25 @@
 package org.spring.servingfiles.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.core.io.PathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+
+@RestController
 public class FileController {
+    @GetMapping(path="/download")
+    public ResponseEntity<Resource> download() {
+        Path path = Path.of("files", "example.txt");
+        Resource file = new PathResource(path);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .header("Content-Disposition", "attachment; filename=" + file.getFilename())
+                .body(file);
+    }
+
 }

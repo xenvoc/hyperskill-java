9ea0341e6a0c42017f61e1187fa67282d4703f67
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-09-07 14:54:17 +0300

    Use Java 11 HTTP Client

--- Assigned but no Solve in IDE button/Java11HTTPClient/src/main/java/org/Spring/Main.java
+++ Assigned but no Solve in IDE button/Java11HTTPClient/src/main/java/org/Spring/Main.java
@@ -1,17 +1,28 @@
 package org.Spring;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        HttpClient httpClient = HttpClient.newHttpClient();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        URI firstWebAddr = URI.create("http://info.cern.ch/hypertext/WWW/TheProject.html");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(firstWebAddr)
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+        } catch (Exception e) {
+            System.out.println("Can't access site, try later");
         }
     }
 }

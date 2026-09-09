7bbaf54b2e404b120aeecab1103c4879d4130699
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-09-09 16:18:45 +0300

    Gen spring for Connecting to a DB w JDBC

---
+++ Assigned but no Solve in IDE button/connectDBwJDBC/src/main/java/org/spring/connectdbwjdbc/ConnectDBwJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.spring.connectdbwjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectDBwJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectDBwJdbcApplication.class, args);
+    }
+
+}

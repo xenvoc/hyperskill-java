3aa2e5aa0f1452635109d1d044c108254a87301e
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-09-15 14:12:32 +0300

    Test w breakpoints

--- Assigned but no Solve in IDE button/BeanVSComponentPasswGen/src/main/java/org/spring/beanvscomponentpasswgen/BeanVsComponentPasswGenApplication.java
+++ Assigned but no Solve in IDE button/BeanVSComponentPasswGen/src/main/java/org/spring/beanvscomponentpasswgen/BeanVsComponentPasswGenApplication.java
@@ -8,6 +8,7 @@ public class BeanVsComponentPasswGenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeanVsComponentPasswGenApplication.class, args);
+        int a = 5;
     }
 
 }

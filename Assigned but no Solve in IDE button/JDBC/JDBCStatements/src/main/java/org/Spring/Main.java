afe0e8464a7e54db8be3279e9002a819e2db0aed
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-09-24 13:54:52 +0300

    Add data source

--- Assigned but no Solve in IDE button/JDBC/JDBCStatements/src/main/java/org/Spring/Main.java
+++ Assigned but no Solve in IDE button/JDBC/JDBCStatements/src/main/java/org/Spring/Main.java
@@ -2,6 +2,10 @@ package org.Spring;
 
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:sqlite:C:/sqlite/westeros.db";
+
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl;
         }
     }
 }

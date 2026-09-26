e271846c68578cbf9618bcf4aed1d085813b44cf
xenvoc <131164834+xenvoc@users.noreply.github.com>
2026-09-26 14:41:25 +0300

    Finish westeros db class

--- Assigned but no Solve in IDE button/JDBC/JDBCStatements/src/main/java/org/Spring/Main.java
+++ Assigned but no Solve in IDE button/JDBC/JDBCStatements/src/main/java/org/Spring/Main.java
@@ -5,7 +5,22 @@ public class Main {
         String url = "jdbc:sqlite:C:/sqlite/westeros.db";
 
         SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl;
+        dataSource.setUrl(url);
+
+        try (Connection con = dataSource.getConnection()) {
+            try (Statement statement = con.createStatement()) {
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS HOUSES(" +
+                        "id INTEGER PRIMARY KEY," +
+                        "name TEXT NOT NULL," +
+                        " words TEXT NOT NULL)");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+
+            }
         }
     }
 }
